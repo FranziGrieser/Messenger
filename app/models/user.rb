6cd83cfbfc2b9659066aa1f8d00f1706caa481a6
FranziGrieser <franzi.grieser@gmail.com>
2019-07-31 22:06:25 +0200

    change validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@
 class User < ApplicationRecord
   validates :username,
     presence: true,
-    length: { minimum: 5, maximum: 30 },
+    length: { minimum: 3, maximum: 30 },
     uniqueness: true
   has_secure_password
   has_many :messages

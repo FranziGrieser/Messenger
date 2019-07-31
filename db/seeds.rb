6eeb3c397909639182d6270aa58b7324385b7615
FranziGrieser <franzi.grieser@gmail.com>
2019-07-31 21:57:52 +0200

    create users and messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.where(username: "Franzi", password_digest: "password").first_or_create
+User.where(username: "Andrew", password_digest: "password").first_or_create
+User.where(username: "Ruby", password_digest: "password").first_or_create
+User.where(username: "Nathan", password_digest: "password").first_or_create
+
+Message.where(body: "How are you guys doing?", user_id: 1).first_or_create
+Message.where(body: "Very good. And you?", user_id: 2).first_or_create
+Message.where(body: "Meeh, kind of bored.", user_id: 1).first_or_create
+Message.where(body: "Haha, maybe you should start programming", user_id: 3).
+  first_or_create
+Message.where(body: "Yeah, then you'll never be bored again.", user_id: 4).
+  first_or_create

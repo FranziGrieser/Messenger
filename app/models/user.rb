88e44bf9372cd20bb140ec9ae2219a8d45619740
FranziGrieser <franzi.grieser@gmail.com>
2019-07-31 21:28:16 +0200

    User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
+  validates :username,
+    presence: true,
+    length: { minimum: 5, maximum: 30 },
+    uniqueness: true
+  has_secure_password
 end

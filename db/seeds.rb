8ab25d941d03b780bc8d78f3ff9468ffd9dc402d
FranziGrieser <franzi.grieser@gmail.com>
2019-07-31 22:06:41 +0200

    rename user

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ User.where(username: "Nathan", password_digest: "password").first_or_create
 Message.where(body: "How are you guys doing?", user_id: 1).first_or_create
 Message.where(body: "Very good. And you?", user_id: 2).first_or_create
 Message.where(body: "Meeh, kind of bored.", user_id: 1).first_or_create
-Message.where(body: "Haha, maybe you should start programming", user_id: 3).
-  first_or_create
-Message.where(body: "Yeah, then you'll never be bored again.", user_id: 4).
-  first_or_create
+Message.
+  where(body: "Haha, maybe you should start programming", user_id: 3).first_or_create
+Message.
+  where(body: "Yeah, then you'll never be bored again.", user_id: 4).first_or_create

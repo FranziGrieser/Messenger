498cbbc36904882c0d07891ac5326b385e4a55b3
FranziGrieser <franzi.grieser@gmail.com>
2019-08-08 21:50:27 +0200

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-User.where(username: "Franzi", password_digest: "password").first_or_create
-User.where(username: "Andrew", password_digest: "password").first_or_create
-User.where(username: "Ruby", password_digest: "password").first_or_create
-User.where(username: "Nathan", password_digest: "password").first_or_create
+User.where(username: "Franzi").first_or_create(password: "password")
+User.where(username: "Andrew").first_or_create(password: "password")
+User.where(username: "Ruby").first_or_create(password: "password")
+User.where(username: "Nathan").first_or_create(password: "password")
 
 Message.where(body: "How are you guys doing?", user_id: 1).first_or_create
 Message.where(body: "Very good. And you?", user_id: 2).first_or_create
